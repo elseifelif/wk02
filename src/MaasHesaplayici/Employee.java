0485a8fa391401d34634d819483d2891ebd51129
elseifelif <141683694+elseifelif@users.noreply.github.com>
2023-10-05 09:29:11 +0300

    second commit

--- src/MaasHesaplayici/Employee.java
+++ src/MaasHesaplayici/Employee.java
@@ -1,95 +1,86 @@
-public class Employee {                                                      
+public class Employee {
     // Değişkenler
     String name;
     int workHours;
     int hireYear;
     double salary;
 
-    // Kurucu metot
-    Employee(String name,int workHours,int hireYear,double salary){      
+    // Constructor (Kurucu) metot
+    Employee(String name,int workHours,int hireYear,double salary){
 
         this.name = name;
         this.workHours = workHours;
         this.hireYear = hireYear;
         this.salary = salary;
-        
+
     }
-    
+
     //vergi hesabı
-   double tax(){                                                                
-        double tax;
-     
-        
+    double tax(){
+        double tax=0;
+
+
         if (salary >= 1000){
             tax =0.03*salary;
-            
+
             return tax;
         }
-        
-        fi (salary<1000)
+
+        if (salary<1000)
         {
             tax =0;
-            return tax;
 
         }
-
-   }
+        return tax;
+    }
     //bonus hesabı
-   double bonus(){
-       double bonusMiktari;
-       
-        if (workHours >40){
-          
-            bonusMiktari = 30*(workHours - 40);
-            
-            return bonusMiktari;
-        } 
-        if (workHours<40)
-        {   bonusMiktari = 0;
-            return bonusMiktari;
-        }
+    double bonus(){
+        double bonusMiktari=0;
 
-   }
+        if (workHours >=40){
+            bonusMiktari = 30*(workHours - 40);
+        }
+        if (workHours<40)
+        {
+            bonusMiktari = 0;
+        }
+        return bonusMiktari;
+    }
     //maaş artışı
-   double raiseSalary(){                                                       
+    double raiseSalary(){
 
         int calismaMiktari = 2021-hireYear;
-        int maasArtisi;
-        
-        if (calismaMiktari<10){
+        double maasArtisi=0;
+
+        if (calismaMiktari<=9){
             maasArtisi=salary*0.05;
-            return maasArtisi;
 
         }
         if (calismaMiktari > 9 && calismaMiktari <20){
             maasArtisi = salary*0.1;
-            return maasArtisi;
 
         }
-        if (calismaMiktari>19)
+        if (calismaMiktari>=20) {
             maasArtisi = salary*0.15;
-            
-            return maasArtisi;
-       }
+         }
+        return maasArtisi;
 
-
-   }
+}
 
     //toString metodu
-   public String toString(){     
-       
-       System.out.println("İsim : "+name);
-       System.out.println("Çalışma Saati : "+workHours);
-       System.out.println("Maaş : "+salary);
-       
-       System.out.println("Başlangıç yılı : "+hireYear);
-       System.out.println("Vergi : "+tax());
-       System.out.println("Bonus : "+bonus());
-       System.out.println("Maaş Artışı : "+ raiseSalary());
-       System.out.println("Vergi ve Bonuslar ile hesaplanan maaş : "+(bonus()-tax()+salary));
-       System.out.println("Toplam Maaş : "+(salary+raiseSalary()));
-       
-       return "";
-   }
+    public String toString(){
 
+        System.out.println("İsim : "+name);
+        System.out.println("Çalışma Saati : "+workHours);
+        System.out.println("Maaş : "+salary);
+
+        System.out.println("Başlangıç yılı : "+hireYear);
+        System.out.println("Vergi : "+tax());
+        System.out.println("Bonus : "+bonus());
+        System.out.println("Maaş Artışı : "+ raiseSalary());
+        System.out.println("Vergi ve Bonuslar ile hesaplanan maaş : "+(bonus()-tax()+salary));
+        System.out.println("Toplam Maaş : "+(salary+raiseSalary()));
+
+        return "";
+    }
 }
